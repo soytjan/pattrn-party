8d2506e411afffb3e3b4bcc9ecfb08f50c07d630
Amanda Tjan <soytjan@gmail.com>
2018-04-08 21:35:07 -0600

    Complete functionality for accordion tabs

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $('.accordion').children('.accordion__tab').first().children('.accordion__tab-link').addClass('accordion__tab-active').next().addClass('is-open').show();
+})
+
+$('.accordion').on('click', 'li > a', function() {
+  console.log(this);
+  if (!$(this).hasClass('active')) {
+    $('.accordion .is-open').removeClass('is-open').hide();
+    $(this).next().toggleClass('is-open').toggle();
+
+    $('.accordion').find('.accordion__tab-active').removeClass('accordion__tab-active');
+    $(this).addClass('accordion__tab-active');
+  }
+})
